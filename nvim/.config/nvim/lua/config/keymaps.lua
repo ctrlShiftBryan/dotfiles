c56647a5d7ed6d75dccc0f142206b5463e5af562
Bryan Arendt <bryanarendt@gmail.com>
2025-07-19 18:05:41 -0400

    feat(nvim): add file path copy keymaps and window bar filename display
    - Add <leader>cf to copy relative file path
    - Add <leader>cF to copy absolute file path
    - Add <leader>cn to copy filename only
    - Add <leader>cP to copy path relative to home directory
    - Display filename in window bar with modification indicator

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,28 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Copy file paths to clipboard
+vim.keymap.set("n", "<leader>cf", function()
+  local path = vim.fn.expand("%:.")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied: "' .. path .. '"')
+end, { desc = "Copy relative file path" })
+
+vim.keymap.set("n", "<leader>cF", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied: "' .. path .. '"')
+end, { desc = "Copy absolute file path" })
+
+vim.keymap.set("n", "<leader>cn", function()
+  local path = vim.fn.expand("%:t")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied: "' .. path .. '"')
+end, { desc = "Copy filename" })
+
+vim.keymap.set("n", "<leader>cP", function()
+  local path = vim.fn.expand("%:~")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied: "' .. path .. '"')
+end, { desc = "Copy path relative to home" })
