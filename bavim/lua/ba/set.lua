be441a5fac826799db497e9cb796235e403185ee
Bryan Arendt <bryanarendt@gmail.com>
2025-10-04 06:34:23 -0400

    feat(bavim): enhance editor settings with comprehensive vim options
    Add complete set of editor preferences including line numbers, tabs,
    search behavior, scrolling, and color settings to improve development
    experience.

--- bavim/lua/ba/set.lua
+++ bavim/lua/ba/set.lua
@@ -1,6 +1,44 @@
+-- Line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Tabs and indentation
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Line wrapping
+vim.opt.wrap = false
+
+-- Backup and swap files
+vim.opt.swapfile = false
+vim.opt.backup = false
+
 -- Persistent undo
 vim.opt.undofile = true
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+-- Search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Colors
+vim.opt.termguicolors = true
+vim.opt.colorcolumn = "80"
+
+-- Scrolling
+vim.opt.scrolloff = 8
+
+-- Sign column
+vim.opt.signcolumn = "yes"
+
+-- Filename characters
+vim.opt.isfname:append("@-@")
+
+-- Update time
+vim.opt.updatetime = 50
 
 -- Use system clipboard
 vim.opt.clipboard = "unnamedplus"
