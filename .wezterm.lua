39f7aba2d72e958c6b62596a7cd0ca9732dbb877
Bryan Arendt <bryanarendt@gmail.com>
2025-10-27 11:53:27 -0400

    chore: enable native macOS fullscreen mode and update comment
    - Enable native_macos_fullscreen_mode to true for improved macOS fullscreen behavior
    - Update fullscreen toggle comment to clarify that it automatically creates new desktop space

--- .wezterm.lua
+++ .wezterm.lua
@@ -22,7 +22,7 @@ config.font_size = 14.0
 config.color_scheme = color_scheme_for_appearance(wezterm.gui.get_appearance())
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
-config.native_macos_fullscreen_mode = false
+config.native_macos_fullscreen_mode = true
 
 -- Transparency
 config.window_background_opacity = 0.80
@@ -36,7 +36,7 @@ config.keys = {
     mods = 'SHIFT',
     action = wezterm.action.SendString '\n',
   },
-  -- Toggle fullscreen with Ctrl+Cmd+f
+  -- Toggle fullscreen with Ctrl+Cmd+f (creates new desktop space automatically)
   {
     key = 'f',
     mods = 'CTRL|CMD',
